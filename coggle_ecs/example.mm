58382a5f8e9da092439a6b9fcf70f507a687ebb2
Gino Valente <gino.valente.code@gmail.com>
2019-12-27 04:33:24 -0800

    Initial commit

---
+++ coggle_ecs/example.mm
@@ -0,0 +1,55 @@
+
+<map version="0.9.0">
+    <node TEXT="Game Engine" FOLDED="false" POSITION="right" ID="5e0568ca68fbc74e9bb7c666" X_COGGLE_POSX="0" X_COGGLE_POSY="0">
+        <edge COLOR="#b4b4b4"/>
+        <font NAME="Helvetica" SIZE="17"/>
+        <node TEXT="Entities" FOLDED="false" POSITION="right" ID="c2876d86e931f9deab2da00f">
+            <edge COLOR="#ebd95f"/>
+            <font NAME="Helvetica" SIZE="15"/>
+            <node TEXT="Player [#](#5d3adc) [#](#78959d)" FOLDED="false" POSITION="right" ID="2fbd2a01b1c71bd12bcbcb6b">
+                <edge COLOR="#ecd966"/>
+                <font NAME="Helvetica" SIZE="13"/>
+                <node TEXT="This is some description." FOLDED="false" POSITION="right" ID="b5b0332157c9287b3640d066">
+                    <edge COLOR="#ecd870"/>
+                    <font NAME="Helvetica" SIZE="13"/>
+                </node>
+            </node>
+            <node TEXT="Alien [#](#78959d) [#](#feee8a)" FOLDED="false" POSITION="right" ID="22db55a2c98a51c71bca1791">
+                <edge COLOR="#ead86c"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+        </node>
+        <node TEXT="Components" FOLDED="false" POSITION="right" ID="abed60d3595a05321d843d5c">
+            <edge COLOR="#efa670"/>
+            <font NAME="Helvetica" SIZE="15"/>
+            <node TEXT="Hostile" FOLDED="false" POSITION="right" ID="feee8a5d3ea26d514852b5db">
+                <edge COLOR="#eea26d"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Transform" FOLDED="false" POSITION="right" ID="78959d52cbc1843fb429b409">
+                <edge COLOR="#ee9d65"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Move" FOLDED="false" POSITION="right" ID="5d3adc4deef6b0da6ebcc899">
+                <edge COLOR="#f09e65"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+        </node>
+        <node TEXT="Systems" FOLDED="false" POSITION="right" ID="36c5720bc10e612f2a18cad1">
+            <edge COLOR="#e68782"/>
+            <font NAME="Helvetica" SIZE="15"/>
+            <node TEXT="Movement System [#](#5d3adc) [#](#78959d)" FOLDED="false" POSITION="right" ID="22e2424c6291b42cd61178e7">
+                <edge COLOR="#e37e7b"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Enemy System [#](#feee8a)" FOLDED="false" POSITION="right" ID="0ed47a23e497face721424d9">
+                <edge COLOR="#e78682"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+        </node>
+        <node TEXT="Art" FOLDED="false" POSITION="left" ID="14bf08663116d2383d6c20f3">
+            <edge COLOR="#e096e9"/>
+            <font NAME="Helvetica" SIZE="15"/>
+        </node>
+    </node>
+</map>
